8e44cd10eb41f9187f8cd9bf6192217a2081a21f
zhifengle <zhifengle@gmail.com>
2024-03-14 11:05:05 +0800

    update: url list

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,8 @@
-use std::{path::PathBuf, fs::File, io::{BufReader, BufRead}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
 
 pub fn get_magnet_list_by_txt(txt: &PathBuf) -> anyhow::Result<Vec<String>> {
     let mut magnet_list = Vec::new();
@@ -6,7 +10,12 @@ pub fn get_magnet_list_by_txt(txt: &PathBuf) -> anyhow::Result<Vec<String>> {
     let reader = BufReader::new(file);
     for line in reader.lines() {
         let line = line?;
-        if line.starts_with("magnet:?xt=urn:btih:") {
+        if line.starts_with("magnet:")
+            || line.starts_with("https://")
+            || line.starts_with("http://")
+            || line.starts_with("ftp://")
+            || line.starts_with("ftps://")
+        {
             magnet_list.push(line.trim_end().to_string());
         }
     }
